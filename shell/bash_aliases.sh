80cfefd7e84100c4db3f57da129d884188e30755
Duane Jennings <github.com@duanejennings.com>
2019-12-19 14:22:24 -0500

    Update and rename aliases.sh to bash_aliases.sh

---
+++ shell/bash_aliases.sh
@@ -0,0 +1,14 @@
+set -o vi
+
+alias ll='ls -alF'
+alias lrt='ls -alFrt'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias dtc='cd /mnt/hgfs/var/ExportToDTC/'
+alias onedrive='cd /mnt/hgfs/OneDrive/'
+alias dropbox='cd /mnt/hgfs/Dropbox/'
+alias datafeed='cd /mnt/hgfs/var/DataFeed/'
+alias yt='youtube-dl'
+alias var='cd /mnt/hgfs/var'
+alias src='cd /mnt/hgfs/OneDrive/Ecommerce/src/'
